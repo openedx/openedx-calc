01337a2f7485ff96575f96fa71765977307db8de
irfanuddinahmad <34648393+irfanuddinahmad@users.noreply.github.com>
2026-09-09 14:07:26 +0500

    fix: remove unnecessary try/except around __version__ (not present pre-migration)

--- src/calc/__init__.py
+++ src/calc/__init__.py
@@ -3,13 +3,8 @@ Ideally, we wouldn't need to pull in all the calc symbols here,
 but courses were using 'import calc', so we need this for
 backwards compatibility
 """
-from importlib.metadata import PackageNotFoundError, version
+from importlib.metadata import version
 
 from .calc import *
 
-try:
-    __version__ = version("openedx-calc")
-except PackageNotFoundError:  # pragma: no cover
-    # Only hit if this package is imported without being installed at all
-    # (e.g. run directly from a source checkout with no metadata available).
-    pass
+__version__ = version("openedx-calc")
